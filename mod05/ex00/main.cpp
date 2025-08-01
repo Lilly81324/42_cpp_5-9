24f1af7de9b2ce53a34997504535c413429d45ab
Lilly Kunne <sikunne@c2r13s2.42wolfsburg.de>
2025-08-01 18:20:15 +0200

    EX00 Improved test cases and their visual

--- mod05/ex00/main.cpp
+++ mod05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:04:24 by sikunne           #+#    #+#             */
-/*   Updated: 2025/08/01 17:21:45 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/08/01 18:19:36 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
  */
 static void	static_inc(Bureaucrat& subject)
 {
-	std::cout << "----------" << std::endl;
+	std::cout << std::endl;
 	try
 	{
 		subject.increment();
@@ -32,7 +32,7 @@ static void	static_inc(Bureaucrat& subject)
  */
 static void	static_dec(Bureaucrat& subject)
 {
-	std::cout << "----------" << std::endl;
+	std::cout << std::endl;
 	try
 	{
 		subject.decrement();
@@ -45,6 +45,7 @@ static void	static_dec(Bureaucrat& subject)
 
 int main(void)
 {
+	std::cout << "---===INVALID CONSTRUCTORS===---" << std::endl;
 	try
 	{
 		Bureaucrat	invalid("Invalid Ingrid", 0);
@@ -61,19 +62,19 @@ int main(void)
 	{
 		std::cerr << ex.what() << std::endl;
 	}
+	std::cout << std::endl << "----===NORMAL DESTRUCTORS===----" << std::endl;
 	Bureaucrat	defaulty;
 	Bureaucrat	high("CEO Carly", 1);
 	Bureaucrat	low("Low Larry", 150);
 	Bureaucrat	average("Average Avery", 75);
 	Bureaucrat	cloned(average);
-	std::cout << "----------" << std::endl;
-	defaulty = high;
+	std::cout << std::endl << "-------=======TESTS======-------" << std::endl;
 	static_inc(high);
 	static_dec(high);
 	static_dec(low);
 	static_inc(low);
-	std::cout << "----------" << std::endl;
+	std::cout << std::endl;
 	average = defaulty = high;
-	std::cout << "----------" << std::endl;
+	std::cout << "-----=====DESTRUCTORS=====------" << std::endl;
 	return (0);
 }

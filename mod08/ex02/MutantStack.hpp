30f06fbc0c682df5c5f16a9ed9e2233b9f4c5259
Lilly Kunne <simkue2@web.de>
2025-10-29 17:55:59 +0100

    ex02 finalized main. Removed -I flag from Makefiles

--- mod08/ex02/MutantStack.hpp
+++ mod08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 16:27:47 by sikunne           #+#    #+#             */
-/*   Updated: 2025/10/28 18:52:59 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/10/29 17:46:02 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,54 @@
 
 #include <iostream>
 #include <stack>
+#include <list>
+#include <vector>
+#include <deque>
 
 template<typename T>
 class MutantStack: public std::stack<T>
 {
 	public:
 		// Available in stack, redeclare here to preceed Templates
-		typedef typename std::stack<T>::container_type		container_type;
-		typedef typename std::stack<T>::value_type			value_type;
-		typedef typename std::stack<T>::size_type			size_type;
-		typedef typename std::stack<T>::reference			reference;
-		typedef typename std::stack<T>::const_reference		const_reference;
+		typedef typename std::stack<T>::container_type			container_type;
+		typedef typename std::stack<T>::value_type				value_type;
+		typedef typename std::stack<T>::size_type				size_type;
+		typedef typename std::stack<T>::reference				reference;
+		typedef typename std::stack<T>::const_reference			const_reference;
 		// Not available in stack, BUT available in stacks container
-		typedef typename container_type::iterator			iterator;
-		typedef typename container_type::const_iterator		const_iterator;
+		typedef typename container_type::iterator				iterator;
+		typedef typename container_type::const_iterator			const_iterator;
+		typedef typename container_type::reverse_iterator		reverse_iterator;
+		typedef typename container_type::const_reverse_iterator	const_reverse_iterator;
 
 		iterator begin()
 			{ return this->c.begin(); }
 		iterator end()
 			{ return this->c.end(); }
+
 		const_iterator begin() const
 			{ return this->c.begin(); }
 		const_iterator end() const
 			{ return this->c.end(); }
+
+		reverse_iterator rbegin()
+			{ return this->c.rbegin(); }
+		reverse_iterator rend()
+			{ return this->c.rend(); }
+
+		const_reverse_iterator rbegin() const
+			{ return this->c.rbegin(); }
+		const_reverse_iterator rend() const
+			{ return this->c.rend(); }
 };
 
 #endif 
+
+// Notes:
+// Relational Operators on containers
+// == true,		if all elements equal AND equal size
+// <  true,		if first unequal elements are <, OR all values are equal and a is smaller size
+// >  true,		if first unequal elements are >, OR all values are equal and b is smaller size
+// <= Negation of >
+// >= Negation of <
+// != Negation of ==
--- mod08/ex02/main.cpp
+++ mod08/ex02/main.cpp
@@ -6,48 +6,145 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 16:27:32 by sikunne           #+#    #+#             */
-/*   Updated: 2025/10/28 19:12:21 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/10/29 17:54:39 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
-#include <list>
-#include <vector>
 
-// MutantStack	list		vector		deque
-// push			push_back	push_back	push_back
-// top			back		back		back
+template<typename T>
+void testIterators(MutantStack<T> &a, const MutantStack<T> &const_a)
+{
+	std::cout << "\nIterate Front to back" << std::endl;
+	typename MutantStack<T>::iterator end = a.end();
+	typename MutantStack<T>::iterator it = a.begin();
+	while (it != end)
+	{
+		std::cout << *it;
+		it++;
+		if (it != end)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	typename MutantStack<T>::const_iterator end2 = const_a.end();
+	typename MutantStack<T>::const_iterator it2 = const_a.begin();
+	while (it2 != end2)
+	{
+		std::cout << *it2;
+		it2++;
+		if (it2 != end2)
+			std::cout << " ";
+	}
+	(void)const_a;
+	std::cout << "\n\nIterate Back to front" << std::endl;
+	typename MutantStack<T>::reverse_iterator end3 = a.rend();
+	typename MutantStack<T>::reverse_iterator it3 = a.rbegin();
+	while (it3 != end3)
+	{
+		std::cout << *it3;
+		it3++;
+		if (it3 != end3)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	typename MutantStack<T>::const_reverse_iterator end4 = const_a.rend();
+	typename MutantStack<T>::const_reverse_iterator it4 = const_a.rbegin();
+	while (it4 != end4)
+	{
+		std::cout << *it4;
+		it4++;
+		if (it4 != end4)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+std::ostream &operator<<(std::ostream &out, const MutantStack<T>& s)
+{
+	typename MutantStack<T>::const_iterator end = s.end();
+	typename MutantStack<T>::const_iterator it = s.begin();
+	while (it != end)
+	{
+		out << *it;
+		it++;
+		if (it != end)
+			out << " ";
+	}
+	return (out);
+}
+
+int	testRelationalOperators(const MutantStack<int> &a, const MutantStack<int> &b)
+{
+	// Introduce our contestants
+	std::cout << std::endl << "Testing: [" << a << "] against [" << b << "]" << std::endl;
+
+	// Fill bitmask based on operations
+	unsigned int result = 0;
+	if (a == b)	result += 1 << 0;
+	if (a != b)	result += 1 << 1;
+	if (a <  b)	result += 1 << 2;
+	if (a <= b)	result += 1 << 3;
+	if (a >  b)	result += 1 << 4;
+	if (a >= b)	result += 1 << 5;
+
+	// Check bitmask for operations
+	const std::string checks[6] = {"==", "!=", "< ", "<=", "> ", ">="};
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << "[" << a << "] " << checks[i] << " [" << b << "] -> ";
+		if (((result >> i) & 1) == 1)
+			std::cout << "true" << std::endl;
+		else
+			std::cout << "false" << std::endl;
+	}
+	return (result);
+}
+
+// !! Important for Eval !!
+
+// MutantStack	stack	list		vector		deque
+// push			push	push_back	push_back	push_back
+// top			top		back		back		back
 
 // Stack takes only deque as Constructor, so another type needs to be specified like this:
 // std::stack<int, std::list<int> > s(mstack);
 
 int main()
 {
-	{ // mandatory
-		MutantStack<int> mstack;
-		mstack.push(5);
-		mstack.push(17);
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
-		std::cout << mstack.size() << std::endl;
-		mstack.push(3);
-		mstack.push(5);
-		mstack.push(737);
-		//[...]
-		mstack.push(0);
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
-		++it;
-		--it;
-		while (it != ite)
-		{
-		std::cout << *it << std::endl;
-		++it;
-		}
-		std::stack<int> s(mstack);
-	}
-	// {
-	// 	std::list<int> mstack
-	// }
-	return 0;
+	MutantStack<int> mstack;
+	MutantStack<int> cpy;
+
+	// push
+	mstack.push(2);
+	mstack.push(4);
+	mstack.push(7);
+	mstack.push(99);
+
+	// empty()
+	if (cpy.empty())
+		std::cout << "Mutant Stack is empty" << std::endl;
+	else
+		std::cout << "Error!" << std::endl;
+
+	// operator= and top()
+	cpy = mstack;
+	if (cpy.top() == mstack.top() && cpy.top() == 99)
+		std::cout << "Copied MutantStack, " << cpy.top() << " at top" << std::endl;
+	else
+		std::cout << "Error!" << std::endl;
+
+	// pop() and size
+	mstack.pop();
+	if (mstack.size() == 3 && cpy.size() == 4)
+		std::cout << "MutantStack was popped" << std::endl;
+	else
+		std::cout << "Error!" << std::endl;
+
+	// Iterators
+	testIterators(cpy, cpy);
+
+	// Relational Operators and std::swap
+	testRelationalOperators(mstack, cpy);
+	return (0);
 }
